cea695657a1d27814d6d9a9531a0f975b2bbfc69
EJ Wolfe <nakoa.sup@proton.me>
2023-07-04 09:58:59 -0500

    Adding reqwest dependency
    Added reqwest dependency and an example of how to do a blocking get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+    request_data();
+}
+
+fn request_data() {
+    let response = reqwest::blocking::get("https://jsonplaceholder.typicode.com/todos/1");
+    let result = match response {
+        Ok(result) => result,
+        Err(error) => panic!("Error retreiving data: {}", error),
+    };
+    let data = match result.text() {
+        Ok(text) => text,
+        Err(error) => panic!("Error parsing text: {}", error),
+    };
+    println!("Data: {}", data);
 }
