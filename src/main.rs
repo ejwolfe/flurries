b507b978a93543d7513096b7a968a843a17a9ed4
EJ Wolfe <nakoa.sup@protonmail.com>
2023-07-10 16:31:00 -0500

    Adding emojis for output
    Gathered emoji for outputting the weather conditions based on the id from the service.

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,63 @@ fn main() {
     println!("{}", &forecast.city.name);
     println!("{}", &forecast.list[0].pop);
 }
+
+enum Conditions {
+    ClearSky,
+    FewClouds,
+    ScatteredClouds,
+    BrokenClouds,
+    RainShower,
+    Rain,
+    Thunderstorm,
+    Snow,
+    Mist,
+}
+
+impl Conditions {
+    fn as_day_str(&self) -> &'static str {
+        match self {
+            Conditions::ClearSky => "☀️",
+            Conditions::FewClouds => "⛅",
+            Conditions::ScatteredClouds => "☁️",
+            Conditions::BrokenClouds => "☁️",
+            Conditions::RainShower => "🌧️",
+            Conditions::Rain => "☔",
+            Conditions::Thunderstorm => "🌩️",
+            Conditions::Snow => "🌨️",
+            Conditions::Mist => "🌫️",
+            _ => "",
+        }
+    }
+    fn as_night_str(&self) -> &'static str {
+        match self {
+            Conditions::ClearSky => "🌑",
+            Conditions::FewClouds => "☁️",
+            Conditions::ScatteredClouds => "☁️",
+            Conditions::BrokenClouds => "☁️",
+            Conditions::RainShower => "🌧️",
+            Conditions::Rain => "☔",
+            Conditions::Thunderstorm => "🌩️",
+            Conditions::Snow => "🌨️",
+            Conditions::Mist => "🌫️",
+            _ => "",
+        }
+    }
+}
+
+fn print_weather_condition(code: i64) -> String {
+    let test = Conditions::ClearSky.as_day_str();
+    match code {
+        200 => String::from("⛈️"),
+        201 => String::from("⛈️"),
+        202 => String::from("⛈️"),
+        210 => String::from("⛈️"),
+        211 => String::from("⛈️"),
+        212 => String::from("⛈️"),
+        221 => String::from("⛈️"),
+        230 => String::from("⛈️"),
+        231 => String::from("⛈️"),
+        232 => String::from("⛈️"),
+        _ => String::from(""),
+    }
+}
